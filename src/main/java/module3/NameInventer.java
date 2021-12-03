a5270a490d8093aa4c022780372948c23531f636
Maja Domin <majadomin@gmail.com>
2021-12-03 19:45:16 +0100

    Added test for empty string

--- src/main/java/module3/NameInventer.java
+++ src/main/java/module3/NameInventer.java
@@ -2,6 +2,9 @@ package module3;
 
 public class NameInventer {
     public String invert(String name) {
+        if (name.isEmpty()) {
+            return "";
+        }
         throw new NullPointerException();
     }
 }
--- src/test/java/module3/NameInventerTest.java
+++ src/test/java/module3/NameInventerTest.java
@@ -1,15 +1,30 @@
 package module3;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class NameInventerTest {
+
+    private  NameInventer nameInventer;
+
+    @BeforeEach
+    void setUp() {
+        nameInventer = new NameInventer();
+    }
+
     @Test
     void shouldThrowExceptionWhenNullProvided() {
-        NameInventer nameInventer = new NameInventer();
         Assertions.assertThrows(NullPointerException.class, () ->{
             nameInventer.invert(null);
         });
+    }
 
+    @Test
+    void shouldReturnEmptyStringWhenEmptyStringGiven() {
+        String inverted = nameInventer.invert("");
+        assertThat(inverted).isEqualTo("");
     }
 }
